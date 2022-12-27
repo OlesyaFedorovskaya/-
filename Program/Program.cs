cd204addd74e1ef52fbae0c4d14edf6830ca9dbe
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-27 05:21:13 +0300

    add int NewArraySize

--- Program/Program.cs
+++ Program/Program.cs
@@ -36,3 +36,14 @@ void PrintArray(string[] array)
     Console.WriteLine("]");
 }
 
+int NewArraySize(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+
